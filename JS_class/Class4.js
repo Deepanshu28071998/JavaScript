0daff431b736b94579f9d02c110a67f7cbbf7441
Deepanshu Jain <deepanshujain949@gmail.com>
2025-02-23 09:37:05 +0530

    Operations

---
+++ JS_class/Class4.js
@@ -0,0 +1,45 @@
+// Operations
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+console.log(2 + 2)
+console.log(2 - 3)
+console.log(2 * 3)
+console.log(2 / 3)
+console.log(2 % 3)
+console.log(2 ** 3)
+
+
+let str1 = "Hello"
+let str2 = "World"
+let str3 = str1 + str2
+console.log(str3)
+
+let str4 = str1 + " " + str2
+console.log(str4)
+
+console.log("1" + 2)        // Output is 12
+console.log(1 + "2")        // Output is 12
+console.log("1" + "2")      // Output is 12
+console.log("1" + 2 + 2)    //// Output is 122
+console.log(1 + 2 + "2")    // Output is 32
+console.log(true)
+console.log(+true)
+// console.log(true+)
+console.log(false)
+console.log(+false)
+// console.log(false+)
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+console.log(num1, num2, num3)
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+
+++gameCounter
+console.log(gameCounter)
