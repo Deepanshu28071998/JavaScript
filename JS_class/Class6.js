1f4ae8c6dc0021ab9ef41f1ff16920c019d040a1
Deepanshu Jain <deepanshujain949@gmail.com>
2025-02-28 20:42:00 +0530

    Data Types summary

---
+++ JS_class/Class6.js
@@ -0,0 +1,46 @@
+//Primitive  data types
+// 7 types
+//(1). String
+//(2). Boolean
+//(3). Null
+//(4). Undefined
+//(5). Number
+//(6). BigInt
+//(7). Symbol
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n
+
+
+
+
+//Non primitive data types (Reference)
+
+// Array
+// Objects
+// Function
+// Date
+// RegExp
+// Set
+// Map
+// WeakSet
+// WeakMap
+
+
+
+// console.log(typeof null); // object
+// console.log(typeof [1, 2, 3]); // object
+// console.log(typeof {a:1, b:2}); // object
+
+
